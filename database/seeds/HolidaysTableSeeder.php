d93e93f3a0c1caa277b001053c94d0b878c25a58
EmanuelaMocci <emanuelamocci2@gmail.com>
2021-11-09 16:13:14 +0100

    Aggiunta dati tabella manualmente

--- database/seeds/HolidaysTableSeeder.php
+++ database/seeds/HolidaysTableSeeder.php
@@ -12,6 +12,18 @@ class HolidaysTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $newHoliday = new Holiday();
+        $newHoliday->code ="mb-100-123";
+        $newHoliday->country ="Madagascar";
+        $newHoliday->city ="Andilana";
+        $newHoliday->hotel_name ="Andilana Beach";
+        $newHoliday->departure_airport ="Roma Fiumicino";
+        $newHoliday->arrival_airport ="Andilana Airport";
+        $newHoliday->departure_date ="2021/02/20";
+        $newHoliday->arrival_date ="2021/02/20";
+        $newHoliday->days =10;
+        $newHoliday->packedge ="Full Board";
+        $newHoliday->price =1200;
+        $newHoliday->save();
     }
 }
